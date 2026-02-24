a4cbe52802670d6ad6b8b22ce94ed583b9ed8f02
legoj15 <legoj15@yahoo.com>
2026-02-24 18:36:31 -0500

    Tiny functionality tweaks

--- src/utils/vrad/vrad.cpp
+++ src/utils/vrad/vrad.cpp
@@ -3118,7 +3118,8 @@ int ParseCommandLine(int argc, char **argv, bool *onlydetail) {
     } else if (!Q_stricmp(argv[i], CMDLINEOPTION_NOVCONFIG)) {
     } else if (!Q_stricmp(argv[i], "-vproject") ||
                !Q_stricmp(argv[i], "-game") ||
-               !Q_stricmp(argv[i], "-insert_search_path")) {
+               !Q_stricmp(argv[i], "-insert_search_path") ||
+               !Q_stricmp(argv[i], "-binroot")) {
       ++i;
     } else if (!Q_stricmp(argv[i], "-FullMinidumps")) {
       EnableFullMinidumps(true);
@@ -3288,6 +3289,8 @@ void PrintUsage(int argc, char **argv) {
       "\n"
       "  -vproject <directory> : Override the VPROJECT environment variable.\n"
       "  -game <directory>     : Same as -vproject.\n"
+      "  -binroot <directory>  : Overrides the base binary path (to find the "
+      "\"bin\\x64\" folder).\n"
       "\n"
       "Other options:\n"
       "  -novconfig      : Don't bring up graphical UI on vproject errors.\n"
